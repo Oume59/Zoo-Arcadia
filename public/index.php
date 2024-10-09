038b499bb3a126d50e77bb8e4271640e98d138c6
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-10-09 12:21:38 +0200

    index principal

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
-// INDEX PRINCIPAL qui va START le Main (config)
+//INDEX PRINCIPAL (point d'entrée de l'App) > charge les classes nécessaires et instancie le routeur principal et démarre le traitement des requêtes
+
+<?php
+//Importation des classes nécessaires
+use App\Autoloader;
+use App\Config\Main;
+
+//Définir une constante avec le dossier racine du projet 
+define('ROOT', dirname(__DIR__));
+
+//Importer l'autolader qui va charger automatiquement les classes
+require_once ROOT . '/Autoloader.php';
+Autoloader::register();
+
+//Instanciation du main(routeur principal)
+$app = new Main;
+
+//START de l'application
+$app->start();
