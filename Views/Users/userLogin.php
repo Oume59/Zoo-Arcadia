c7326afb3325baf16c8ea37296326e917551ea88
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-10-28 16:22:31 +0100

    modif formulaire+ajout css

--- Views/Users/userLogin.php
+++ Views/Users/userLogin.php
@@ -1,12 +1,33 @@
+<?php
+echo '<link rel="stylesheet" href="/assets/css/login.css">';
+?>
+
 <!-- Formulaire de connexion user (nav bar) + faire un echo php du link css quand il sera ok -->
-<form action="/User/login" method="POST">
-    <div>
-        <label for="email">Email STAFF :</label>
-        <input type="email" id="email" name="email" required>
+<div class="vide"></div>
+
+<div id="background">
+    <div id="form-ui">
+        <form action="/User/connexion" method="post" id="form">
+        <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+            <div id="form-body">
+                <div id="welcome-lines">
+                    <div id="welcome-line-1">STAFF</div>
+                    <div id="welcome-line-2">Bienvenue</div>
+                </div>
+                <div id="input-area">
+                    <div class="form-inp">
+                        <input placeholder="Email" type="text" name="email">
+                        <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token'], ENT_QUOTES, 'UTF-8'); ?>">
+                    </div>
+                    <div class="form-inp">
+                        <input placeholder="Mot de passe" type="password" name="password">
+                    </div>
+                </div>
+                <div id="submit-button-cvr">
+                    <button id="submit-button" type="submit">Connexion</button>
+                </div>
+                <div id="bar"></div>
+            </div>
+        </form>
     </div>
-    <div>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
-    </div>
-    <button type="submit">Connexion</button>
-</form>
+</div>
