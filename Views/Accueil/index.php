d940212cc0b665be52a32aab3aa903edc603ad58
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-06 23:04:10 +0100

    image + h1 en background de mon accueil

---
+++ Views/Accueil/index.php
@@ -0,0 +1,9 @@
+<!-- h1 ACCUEIL -->
+
+<link rel="stylesheet" href="/assets/css/accueil.css">
+
+<img class="imgaccueil" src="/assets/img/tigreaccueil.jpg" alt="" />
+
+<div class="title d-flex justify-content-center">
+    <h1>PLONGEZ AU COEUR DE LA NATURE SAUVAGE</h1>
+</div>
