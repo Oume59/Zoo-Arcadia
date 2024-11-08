7d93f03e629f236080bce49930732f35b55c5116
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-08 23:38:36 +0100

    Add fichier liste des utilisateurs

---
+++ Views/Dashboard/listUsers.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="/assets/css/listDashboard.css">
+
+<p>Liste des Utilisateurs</p>
+
+<table style="border-collapse: collapse; width: 100%;">
+    <thead>
+        <tr>
+            <th>Nom d'utilisateur</th>
+            <th>Email</th>
+            <th>Rôle</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($user->username); ?></td>
+                <td><?php echo htmlspecialchars($user->email); ?></td>
+                <td><?php echo htmlspecialchars($user->role); ?></td>
+                <td>
+                    <a href="ListUsers/edit/<?php echo $user->id; ?>">Éditer l'utilisateur</a> |
+                    <a href="ListUsers/delete/<?php echo $user->id; ?>?csrf_token=<?php echo $_SESSION['csrf_token']; ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?');">Supprimer</a>
+                </td>
+
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
