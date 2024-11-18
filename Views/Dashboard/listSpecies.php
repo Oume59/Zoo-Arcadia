428a77899af1b5b04c80c6b198946387ceb33161
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-18 23:42:48 +0100

    créa form liste des especes

---
+++ Views/Dashboard/listSpecies.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="/assets/css/listDashboard.css">
+
+<p>Liste des Espèces</p>
+
+<table style="border-collapse: collapse; width: 100%;">
+    <thead>
+        <tr>
+            <th>Nom de l'espèce</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($species as $item): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($item->species); ?></td>
+                <td>
+                    <a href="/ListSpecies/edit/<?php echo $item->id; ?>">Éditer l'espèce</a> |
+                    <a href="/ListSpecies/delete/<?php echo $item->id; ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cette espèce ?');">Supprimer</a>
+                </td>
+
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
