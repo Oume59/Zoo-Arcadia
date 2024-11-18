a426ccfbff2a3c14d5ffafe50262eb508ddb005a
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-18 23:41:43 +0100

    cré du form editer une espece

---
+++ Views/Dashboard/editSpecies.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="/assets/css/editDashboard.css">
+
+<p>Éditer l'espèce</p>
+
+<form action="/ListSpecies/edit/<?php echo $species->id ?>" method="POST">
+    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+
+    <label>Nom de l'espèce :</label>
+    <input type="text" id="species" name="species" value="<?php echo htmlspecialchars($species->species); ?>" required>
+    <br>
+
+    <button type="submit">Enregistrer les modifications</button>
+</form>
