f6731b340be91f1ff631e7d2f9cd542bef326648
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-19 22:21:17 +0100

    créa form modification d un habitat ou suppression

---
+++ Views/Dashboard/editHabitats.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="/assets/css/editDashboard.css">
+
+<form action="/ListHabitats/edit/<?php echo $habitat->id; ?>" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+
+    <label>Nom de l'habitat :</label>
+    <input type="text" name="name" value="<?php echo htmlspecialchars($habitat->name); ?>" required>
+    <br>
+
+    <label>Description :</label>
+    <textarea name="description" required><?php echo htmlspecialchars($habitat->description); ?></textarea>
+    <br>
+
+    <label>Image actuelle :</label>
+    <br>
+    <?php if (!empty($habitat->img)): ?>
+        <img src="/assets/img/<?php echo htmlspecialchars($habitat->img); ?>" alt="Image de l'habitat" style="max-width: 150px; max-height: 150px;">
+    <?php else: ?>
+        <p>Aucune image disponible</p>
+    <?php endif; ?>
+    <br>
+
+    <label>Modifier l'image :</label>
+    <input type="file" name="image" accept="image/*">
+    <br>
+
+    <button type="submit">Enregistrer les modifications</button>
+</form>
