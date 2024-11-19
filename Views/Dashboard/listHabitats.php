bbbbdc7983b16ea9f0579d5b18514fe9521dfba6
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-11-19 22:20:23 +0100

    add form list habitats

---
+++ Views/Dashboard/listHabitats.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="/assets/css/listDashboard.css">
+
+<p>Liste des Habitats</p>
+
+<table style="border-collapse: collapse; width: 100%;">
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Description</th>
+            <th>Photo</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($habitats as $habitat): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($habitat->name); ?></td>
+                <td><?php echo htmlspecialchars($habitat->description ?? 'Non défini'); ?></td>
+                <td>
+                    <?php if (!empty($habitat->img)): ?>
+                        <img src="/assets/img/<?php echo htmlspecialchars($habitat->img); ?>" alt="Photo de l'habitat" style="max-width: 50px; max-height: 50px;">
+                    <?php else: ?>
+                        Pas d'image
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <a href="/ListHabitats/edit/<?php echo $habitat->id; ?>">Éditer l'habitat</a> |
+                    <a href="/ListHabitats/delete/<?php echo $habitat->id; ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet habitat ?');">Supprimer</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
