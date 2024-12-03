ae3febb5bdef300cc1807638e3f4c1cc34d0347e
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-03 17:11:42 +0100

    maj restructuraton du code (bien placer mes setters en dessous de mes getters correspondants) et suppression commentaires inutiles

--- Models/SpeciesModel.php
+++ Models/SpeciesModel.php
@@ -13,6 +13,7 @@ class SpeciesModel extends Model
     }
 
     // Récupère toutes les espèces dispo dans la table 'Species'
+    // Setters pour definir/modifier et Getters pour obtenir :
     public function getSpecies()
     {
         return $this->req('SELECT * FROM Species')->fetchAll();
