ed83b7c7fbb7d6b7a6ef6637b40aba8ba55e6415
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-04 23:10:34 +0100

    maj pour rediriger vesr la vue des animaux et habitats visiteurs

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -8,7 +8,19 @@ class MainController extends Controller
     // Méthode pour afficher page d'accueil
     public function index()
     {
-        // Renvoyer la vue d'accueil
+        // Renvoyer la vue d'accueil services visiteurs
         $this->render("Accueil/index");
+    }
+
+    // Méthode pour afficher la vue des visiteurs pour les animaux
+    public function animauxVisiteur()
+    {
+        $this->render("ViewAnimaux/index");
+    }
+    // Méthode pour afficher la page des habitats
+    public function habitatsVisiteurs()
+    {
+        // Renvoyer la vue Habitats visiteurs
+        $this->render("Habitats/index");
     }
 }
