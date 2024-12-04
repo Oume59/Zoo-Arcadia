6f8f6bd4bf280d3982e4aaedf6a0520b66a61e7c
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-04 23:11:54 +0100

    créa front vue visiteur animaux

---
+++ Views/ViewAnimaux/index.php
@@ -0,0 +1,64 @@
+<section id="carouselanimaux" class="carousel slide my-4" data-bs-ride="carousel">
+    <div class="carousel-animals">
+        <div class="carousel-item active">
+            <img src="Public/assets/img/animvisiteurs.jpg" class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+            <img src="Public/assets/img/babouin.jpg" class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+            <img src="Public/assets/img/flamantmarais.jpg" class="d-block w-100" alt="">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselanimaux" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselanimaux" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</section>
+
+<h2>DÉCOUVREZ NOS ESPÈCES FASCINANTES</h2>
+
+<section class="text-center my-5">
+    <div class="rounded-circle mx-auto bg-light text-dark d-flex align-items-center justify-content-center" style="width: 300px; height: 300px;">
+        <p class="descriptanimals">
+            Chaque habitat est peuplé d'animaux <br>
+            spécifiques à leur écosystème : <br>
+            Venez observer les lions, véritables rois de la savane, ainsi que les girafes et leurs longs cous gracieux, ou encore les éléphants, symbole de force et de sagesse.
+            Les tigres majestueux rôdent dans la jungle dense, tandis que des familles de singes agiles jouent dans les arbres. Vous y découvrirez également des oiseaux tropicaux colorés qui égayent cet environnement verdoyant.
+            Les crocodiles, maîtres des eaux, veillent sur leur territoire, accompagnés d'hippopotames imposants et de tortues qui se prélassent au soleil.
+            Pour chaque animal, vous trouverez des rapports vétérinaires détaillés concernant leur santé, leur alimentation quotidienne et les soins prodigués.
+            Pour chaque animal, vous trouverez des rapports détaillés concernant leur santé, leur alimentation quotidienne et les soins prodigués.
+        </p>
+    </div>
+</section>
+
+<section class="container my-5">
+    <div class="row g-4">
+        <div class="col-md-6 col-lg-4">
+            <div class="card">
+                <img src="/assets/img/tiger.jpg" class="card-img-top" alt="Un tigre de Bengale">
+                <div class="card-body">
+                    <h5 class="card-title">Prénom : JAVA</h5>
+                    <p class="card-text">Espèce : Tigre du Bengale</p>
+                    <p class="card-text">État de santé : Bonne santé générale</p>
+                    <a href="/Reports/1" class="btn btn-primary">+ d'infos</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+            <div class="card">
+                <img src="/assets/img/python.jpg" class="card-img-top" alt="Un python réticulé">
+                <div class="card-body">
+                    <h5 class="card-title">Prénom : MONTY</h5>
+                    <p class="card-text">Espèce : Python réticulé</p>
+                    <p class="card-text">État de santé : Changement de peau</p>
+                    <a href="/Reports/2" class="btn btn-primary">+ d'infos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
