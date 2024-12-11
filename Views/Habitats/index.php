7d11677e4fb3f498d8b5cba1555b2c4d182b978e
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-11 18:04:13 +0100

    cré view habitats visiteurs

--- Views/Habitats/index.php
+++ Views/Habitats/index.php
@@ -1,1 +1,68 @@
-<h2>VOYAGE À TRAVERS LES HABITATS DU ZOO</h2>
+<link rel="stylesheet" href="/assets/css/default.css">
+<link rel="stylesheet" href="/assets/css/habitats.css">
+
+
+<!-- CAROUSEL -->
+<section id="carousel-habitats" class="py-4">
+    <div id="carouselHabitats" class="carousel slide mx-auto" data-bs-ride="carousel" style="max-width: 80%;">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/assets/img/animvisiteurs.jpg" class="d-block w-100" alt="">
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/img/flamantmarais.jpg" class="d-block w-100" alt="">
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/img/elephantsavane.jpg" class="d-block w-100" alt="">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselHabitats" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Précédent</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselHabitats" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Suivant</span>
+        </button>
+    </div>
+</section>
+
+<!-- TITLE -->
+<div class="title d-flex justify-content-center">
+    <h2>VOYAGE À TRAVERS LES HABITATS DU ZOO</h2>
+</div>
+
+<!-- CARDS HABITATS -->
+<section id="cards-habitats" class="py-5">
+    <div class="container">
+        <div class="row g-3">
+
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="/assets/img/tigre1.jpg" class="card-img-top" alt="Tigre du Bengale">
+                    <div class="card-body">
+                        <a href="#" class="btn btn-primary">SAVANE</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="/assets/img/seprentmarais1.jpg" class="card-img-top" alt="Python réticulé">
+                    <div class="card-body">
+                        <a href="#" class="btn btn-primary">MARAIS</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="/assets/img/giraffe.jpg" class="card-img-top" alt="Girafe du Niger">
+                    <div class="card-body">
+                        <a href="#" class="btn btn-primary">JUNGLE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
