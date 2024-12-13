ade5f34d1d67bb09c73833c5d0f6f3bef5c6c420
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-13 10:47:17 +0100

    verif lien bootstrap

--- Views/Default.php
+++ Views/Default.php
@@ -150,7 +150,7 @@
     <!-- SCRIPTS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="/assets/js/modals.js"></script> <!-- Assurez-vous que le chemin est correct -->
+    <script src="/assets/js/modals.js"></script> 
 
 </body>
 
