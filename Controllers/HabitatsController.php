1638a0935eb14b2489b79252de679b00a6a787eb
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-23 23:20:50 +0100

    MAJ controller pour intégrer view des cards

--- Controllers/HabitatsController.php
+++ Controllers/HabitatsController.php
@@ -12,4 +12,22 @@ class HabitatsController extends Controller
         // Renvoyer la vue d'Animaux visiteurs
         $this->render("Habitats/index");
     }
+
+    public function showHabitatsCards($id)
+    {
+        $habitatsModel = new HabitatsModel();
+        $habitat = $habitatsModel->find($id); // Récupère les données de l'habitat via son ID
+
+        if (!$habitat) {
+            // Si aucun habitat trouvé, renvoie une erreur 404
+            http_response_code(404);
+            echo "Habitat introuvable.";
+            exit;
+        }
+
+        // Rendu de la vue avec les données de l'habitat
+        $this->render('Habitats/showHabitatsCards', [
+            'habitat' => $habitat
+        ]);
+    }
 }
