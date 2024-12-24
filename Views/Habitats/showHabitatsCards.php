84ac8e7e08d0ddc3471bf1e3f19d04fb433597ac
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2024-12-24 17:21:45 +0100

    view cards habitats with description and animals list OK

--- Views/Habitats/showHabitatsCards.php
+++ Views/Habitats/showHabitatsCards.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 $css = "showHabitatsCards";
 ?>
 
 <div class="container py-5">
     <h3 class="text-center"><?= htmlspecialchars($habitat->name); ?></h3>
 
-    <div class="row">
+    <div class="row mb-4">
         <div class="col-md-6">
             <img src="/assets/img/<?= htmlspecialchars($habitat->img); ?>" class="img-fluid rounded" alt="<?= htmlspecialchars($habitat->name); ?>">
         </div>
@@ -13,5 +13,25 @@ $css = "showHabitatsCards";
             <h3>Description</h3>
             <p><?= nl2br(htmlspecialchars($habitat->description)); ?></p>
         </div>
+    </div>
+
+    <h3 class="text-center">Animaux dans cet habitat</h3>
+    <div class="row">
+        <?php if (!empty($animals)): ?>
+            <?php foreach ($animals as $animal): ?>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="/assets/img/<?= htmlspecialchars($animal->img); ?>" class="card-img-top" alt="<?= htmlspecialchars($animal->name); ?>">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?= htmlspecialchars($animal->name); ?></h5>
+                            <p class="card-text">Espèce : <?= htmlspecialchars($animal->species_name ?? 'Inconnue'); ?></p>
+                            <a href="/Reports/showAnimalReports/<?= htmlspecialchars($animal->id); ?>" class="btn btn-primary">Voir les rapports</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p class="text-center">Aucun animal trouvé dans cet habitat.</p>
+        <?php endif; ?>
     </div>
 </div>
