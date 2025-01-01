d4065204fa811d233575ea61187a4c05c2e10db1
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-01 20:21:09 +0100

    ajout partie SERVICES sur la page d'accueil

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -10,9 +10,20 @@ class MainController extends Controller
     // Méthode pour afficher page d'accueil
     public function index()
     {
-        // Renvoyer la vue d'accueil services visiteurs
         $servicesModel = new ServicesModel();
         $services = $servicesModel->findAll(); // Récupération de la liste des services
-        $this->render("Accueil/index");
+        $this->render("Accueil/index", [
+            'services' => $services // Passage de la variable à la vue
+        ]);
+    }
+
+    public function accueil()
+    {
+        $servicesModel = new ServicesModel();
+        $services = $servicesModel->findAll(); // Récupère tous les services
+    
+        $this->render('accueil', [
+            'services' => $services // Renvoi à la vue
+        ]);
     }
 }
