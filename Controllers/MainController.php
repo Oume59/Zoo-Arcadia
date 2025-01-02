3fd328bc6c484f8419d364d1f840148fdbff9895
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-02 23:13:18 +0100

    Partir View descriptif des services sucess

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -21,9 +21,28 @@ class MainController extends Controller
     {
         $servicesModel = new ServicesModel();
         $services = $servicesModel->findAll(); // Récupère tous les services
-    
+
         $this->render('accueil', [
             'services' => $services // Renvoi à la vue
+        ]);
+    }
+
+    public function showServicesCards($id)
+    {
+        $servicesModel = new ServicesModel();
+
+        // Récupère les données du service via son ID
+        $service = $servicesModel->find($id);
+        if (!$service) {
+            // Si aucun service trouvé, renvoie une erreur 404
+            http_response_code(404);
+            echo "Service introuvable.";
+            exit;
+        }
+
+        // Rendu de la vue avec les données des services
+        $this->render('Accueil/showServicesCards', [
+            'service' => $service,
         ]);
     }
 }
