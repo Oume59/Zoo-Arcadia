774dd98a4ff7b200f370e2b8ace280a882641e8a
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-10 22:28:52 +0100

    CRUD NOSQL OK

---
+++ Models/MongoModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+require_once __DIR__ . '/../../vendor/autoload.php'; // Charge l'autoloader pour inclure la dépendance externe MongoDB
+
+use App\Config\MongoDbConfig; // USE la class de config MongoDB pour se connecter à la BDD
+use MongoDB\BSON\ObjectId; // USE type BSON ObjectId pour manipuler les identifiants MongoDB
+
+class MongoModel
+{
+    protected $collection;
+
+    //Initialise la connexion à la collection MongoDB spécifique
+    public function __construct($collection)
+    {
+        // Sélectionne la collection passée en param à partir de la configuration MongoDB
+        $this->collection = MongoDbConfig::getDatabase()->selectCollection($collection);
+    }
+
+    // CRUD NO SQL
+
+    public function create(array $data): string
+    {
+        $result = $this->collection->insertOne($data); // Insère le document dans la collection
+        return (string) $result->getInsertedId(); // Retourne l'identifiant du document inséré sous forme de chaîne
+    }
+
+    public function find(string $id): ?array
+    {
+        $result = $this->collection->findOne(['_id' => new ObjectId($id)]); // Recherche par ObjectId
+        return $result ? $result->getArrayCopy() : null; // Retourne le doc en tableau PHP ou null si non existant
+    }
+
+    //Récupère tous les documents de la collection et liste de tous les doc en tableau PHP.
+    public function findAll(): array
+    {
+        return $this->collection->find()->toArray();
+    }
+
+    public function update(string $id, array $data): int
+    {
+        $result = $this->collection->updateOne(
+            ['_id' => new ObjectId($id)], // ID du doc
+            ['$set' => $data] // MAJ les champs spécifiés
+        );
+        return $result->getModifiedCount(); // Retourne le nombre de documents modifiés
+    }
+
+    public function delete(string $id): int
+    {
+        $result = $this->collection->deleteOne(['_id' => new ObjectId($id)]); // Supprime le document par son identifiant
+        return $result->getDeletedCount(); // Retourne le nombre de documents supprimés
+    }
+}
