92505a8da195e3e37e84182a32dce957dc43057e
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-12 22:32:11 +0100

    MAJ suite à l'ajout des AVIS

--- Public/assets/js/modals.js
+++ Public/assets/js/modals.js
@@ -1,17 +1,31 @@
 function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
+  document.getElementById(modalId).style.display = "block";
 }
 
 function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
+  document.getElementById(modalId).style.display = "none";
 }
 
 // Fermer la modale si l'utilisateur clique en dehors de celle-ci
-window.onclick = function(event) {
-    const modals = document.getElementsByClassName('modal');
-    for (let i = 0; i < modals.length; i++) {
-        if (event.target == modals[i]) {
-            modals[i].style.display = "none";
-        }
+window.onclick = function (event) {
+  const modals = document.getElementsByClassName("modal");
+  for (let i = 0; i < modals.length; i++) {
+    if (event.target == modals[i]) {
+      modals[i].style.display = "none";
     }
-}
+  }
+};
+
+// Gestion  des étoiles REVIEWS
+document.querySelectorAll('.star').forEach(star => {
+    star.addEventListener('click', function () {
+        const stars = Array.from(this.parentNode.children);
+        const index = stars.indexOf(this);
+
+        stars.forEach((s, i) => {
+            s.style.color = i <= index ? 'gold' : 'lightgray'; 
+        });
+
+        document.querySelector('#note').value = index + 1; // Enregistre la note sélectionnée
+    });
+});
