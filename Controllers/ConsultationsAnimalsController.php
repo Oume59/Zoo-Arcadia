97afb6e9ab7f8a9cc5456327f966c8ca0d878fdb
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-16 23:19:40 +0100

    commencement de la partie « consultations clics/animal : créa du Controller apres avoir creer la collection sur mongoDb

---
+++ Controllers/ConsultationsAnimalsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ConsultationsAnimalsModel;
+
+class ConsultationsAnimalsController extends Controller
+{
+    private $consultationsAnimalsModel;
+
+    public function __construct()
+    {
+        $this->consultationsAnimalsModel = new ConsultationsAnimalsModel();
+    }
+
+    // Incrémenter le compteur de views pour un animal
+    public function increment($animal)
+    {
+        $this->consultationsAnimalsModel->incrementViewCount($animal);
+        echo json_encode(['message' => "Compteur de '$animal' mis à jour."]);
+    }
+
+    // Obtenir les consult par animal donné
+    public function getCount($animal)
+    {
+        $count = $this->consultationsAnimalsModel->getViewCount($animal);
+        echo json_encode(['views_consultations' => $count]);
+    }
+
+    // Obtenir toutes les consultations (pour le dashboard)
+    public function getAllCounts()
+    {
+        $counts = $this->consultationsAnimalsModel->getAllViewCounts();
+        echo json_encode($counts);
+    }
+}
