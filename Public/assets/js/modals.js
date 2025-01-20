6d786d01f3d3e62047f03be9e4915e8d4aed7204
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-20 23:59:29 +0100

    MAJ

--- Public/assets/js/modals.js
+++ Public/assets/js/modals.js
@@ -1,27 +1,45 @@
+// partie FUTTER mentions legales...
+
 function openModal(modalId) {
   document.getElementById(modalId).style.display = "block";
-}
+} 
 
 function closeModal(modalId) {
   document.getElementById(modalId).style.display = "none";
-}
+} 
 
 // Fermer la modale si l'utilisateur clique en dehors de celle-ci
-window.onclick = function (event) {
+window.addEventListener("click", (event) => {
   const modals = document.getElementsByClassName("modal");
   for (let i = 0; i < modals.length; i++) {
     if (event.target == modals[i]) {
       modals[i].style.display = "none";
     }
   }
-};
+});
 
-// Gestion  des étoiles REVIEWS
-const ratingStars = document.querySelectorAll('.rating input');
-const hiddenNoteInput = document.getElementById('note');
+// Partie Gestion des étoiles REVIEWS
+document.querySelectorAll('.modal .rating input[type="radio"]').forEach((input) => {
+  input.addEventListener("change", () => {
+    const value = parseInt(input.value); // Récupère la valeur de l'étoile sélectionnée
+    const labels = Array.from(document.querySelectorAll(".modal .rating label")); // Sélectionne les labels
 
-ratingStars.forEach((star) => {
-    star.addEventListener('change', () => {
-        hiddenNoteInput.value = star.value;
+    // Réinitialise toutes les étoiles
+    labels.forEach((label, index) => {
+      if (index + 1 <= value) {
+        label.classList.add("star-active"); // Ajoute la classe active
+        label.classList.remove("star-inactive"); // Supprime la classe inactive
+      } else {
+        label.classList.add("star-inactive"); // Ajoute la classe inactive
+        label.classList.remove("star-active"); // Supprime la classe active
+      }
     });
+  });
+});
+
+// Enregistre la note sélectionnée ds l'input caché
+document.querySelectorAll(".modal .rating input").forEach((input) => {
+  input.addEventListener("change", function () {
+    document.querySelector("#note").value = this.value; // MAJ la valeur cachée
+  });
 });
