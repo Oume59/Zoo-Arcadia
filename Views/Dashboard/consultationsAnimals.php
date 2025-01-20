f2c4dfa997a2025b038fab442689b4a0e686cea4
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-01-21 00:00:21 +0100

    MAJ 17 (weekend du 18 et 19 grippe donc mis de côté) reprise le 20/01/2025 : beaucoup de test pour faire fonctionner le compteur de vues / clics sur card animals : se termine avec un succes mais une error car le compteur s’incrémente en +2 clic /clic sur card.. debug en cours

---
+++ Views/Dashboard/consultationsAnimals.php
@@ -0,0 +1,22 @@
+<?php
+$css = "listDashboard";
+?>
+
+<h3>Consultation des Clics/cartes des animaux du Zoo :</h3>
+
+<table>
+    <thead>
+        <tr>
+            <th>Animal</th>
+            <th>Nombre de consultations</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($counts as $item): ?>
+            <tr>
+                <td><?= htmlspecialchars($item['animals']) ?></td>
+                <td><?= htmlspecialchars($item['views_consultations']) ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
