a76aeb412d848846a9cf85999abbd0d267035792
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-02-04 11:05:38 +0100

    maj bootstrap

--- Views/Contact/index.php
+++ Views/Contact/index.php
@@ -13,30 +13,30 @@ $css = "editDashboard";
 <form action="/Contact/sendContactMail" method="POST">
     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token']; ?>">
 
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" placeholder="Votre nom" required>
+    <div class="mb-3">
+        <label for="nom" class="form-label">Nom :</label>
+        <input type="text" id="nom" name="nom" class="form-control" placeholder="Votre nom" required>
     </div>
 
-    <div>
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" placeholder="Votre prénom" required>
+    <div class="mb-3">
+        <label for="prenom" class="form-label">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" class="form-control" placeholder="Votre prénom" required>
     </div>
 
-    <div>
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" placeholder="Votre email" required>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email :</label>
+        <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" required>
     </div>
 
-    <div>
-        <label for="message">Message, objet de votre demande :</label>
-        <textarea id="message" name="message" placeholder="Votre message" rows="5" required></textarea>
+    <div class="mb-3">
+        <label for="message" class="form-label">Message, objet de votre demande :</label>
+        <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="5" required></textarea>
     </div>
 
-    <button type="submit">Envoyer</button>
+    <button type="submit" class="btn btn-primary w-100">Envoyer</button>
 </form>
 
 <!-- BUTTON RETURN -->
 <div class="centered">
-<a href="/Main" class="btn-back">QUITTER</a>
+    <a href="/Main" class="btn-back">QUITTER</a>
 </div>
