fd9180d9980e91871050e6ae1c6169ac7097298c
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-02-04 11:02:29 +0100

    maj add button return

--- Views/Dashboard/consultationsAnimals.php
+++ Views/Dashboard/consultationsAnimals.php
@@ -20,3 +20,8 @@ $css = "listDashboard";
         <?php endforeach; ?>
     </tbody>
 </table>
+
+<!-- BUTTON RETURN -->
+<div class="centered">
+    <a href="/Dashboard" class="btn-back">QUITTER</a>
+</div>
