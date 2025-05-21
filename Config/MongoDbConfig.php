119fb7b623ede88a63bd590ee11916b027aac461
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-05-21 19:26:16 +0200

    controle KF pour meilleure indentation

--- Config/MongoDbConfig.php
+++ Config/MongoDbConfig.php
@@ -8,20 +8,36 @@ use MongoDB\Database;
 class MongoDbConfig
 {
     private static ?MongoDbConfig $instance = null;
-    private Client $client;
-    private Database $db;
+    private Client $client; // client Mongo (connexion)
+    private Database $db; // BDD séléctionnée
 
-public function __construct(){$this->client = $this->connect();$this->db = $this->client->selectDatabase($_ENV['MONGODB_DB']);}
+    public function __construct() // initialise la connexion et sélectionne la base
+    {
+        $this->client = $this->connect(); // Appelle la méthode connect pour créer le client MongoDB
+        $this->db = $this->client->selectDatabase($_ENV['MONGODB_DB']); // Sélect la BDD définie dans .env
+    }
 
-protected function connect(){
-    try {// Connect to MongoDB Atlas via URI
-        return new Client($_ENV['MONGODB_URI']);} catch (\Exception $e) {
-        die("Erreur de connexion à MongoDB : " . $e->getMessage());}}
+    protected function connect()  // Connect MongoDB via l'URI
+    {
+        try { 
+            return new Client($_ENV['MONGODB_URI']);
+        } catch (\Exception $e) {
+            die("Erreur de connexion à MongoDB : " . $e->getMessage());
+        }
+    }
 
-public static function getInstance(): MongoDbConfig {
-    if (self::$instance === null) {self::$instance = new MongoDbConfig();}
-    return self::$instance;}
+    //Méthode statique pour obtenir l'instance unique de la connexion "PATTERN SINGLETON"
+    public static function getInstance(): MongoDbConfig
+    {
+        if (self::$instance === null) { // si pas d'instance : création
+            self::$instance = new MongoDbConfig();
+        }
+        return self::$instance;
+    }
 
-public function getDatabase(): Database {
-    return $this->db;}
+    // permet de récup la BDD connecté pour l'utiliser dans l'app
+    public function getDatabase(): Database
+    {
+        return $this->db;
+    }
 }
