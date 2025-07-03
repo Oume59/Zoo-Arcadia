d27a63b85e481de708cdcc73c2deae8d108df151
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-07-03 19:29:52 +0200

    sup des commenatires inutiles

--- Autoloader.php
+++ Autoloader.php
@@ -1,27 +1,26 @@
 <?php
 
-//Utilisation de l'Autoloader pour charger automatiquement les classes nécessaires sans avoir à inclure à la mano chaque fichier. 
-//Namespace de la classe qui va permettre d'organiser le code et éviter les conflits de noms
+//Namespace de la class qui va permettre d'organiser le code et éviter les conflits de noms
 namespace App;
 
 class Autoloader
 {
 
-    //Méthode statique donc pas besoin de créer une instance de classe pour être appelée + Fonction qui va servir à autoloader les classes lors de leur instanciation. 
+    // Enregistrer l autoloader personnalisé + celui de Composer
     public static function register()
     {
-        require_once __DIR__ . "/vendor/autoload.php";
-        spl_autoload_register([__CLASS__, 'Autoload']);
+        require_once __DIR__ . "/vendor/autoload.php"; // autoload de Composer
+        spl_autoload_register([__CLASS__, 'Autoload']); // si une class inconnue est utilisée, prendre function Autoload pour trouver et inclure son fichier
     }
 
-    //Méthode qui SI elle trouve une classe, elle la charge. SINON, elle affiche un message d'erreur.
+    //  Charge automatiquement une class du namespace "App" si elle existe SINON erro affiché dans les logs
     public static function Autoload($class)
     {
-        if (strpos($class, __NAMESPACE__) === 0) {
-            $class = str_replace(__NAMESPACE__ . '\\', '', $class); // Const spéciale qui renvoie le nom complet de la classe actuelle y compris le namespace.
-            $class = str_replace('\\', '/', $class); //str_replace : fonction qui va remplacer toutes les occurences d'une chaîne par 1 autre dans 1 chaîne de données.
-            $file = __DIR__ . '/' . $class . '.php'; //Peux importe où le script est exécuté, la const DIR me donne le chemin vers le dossier du fichier où DIR est utilisé.
-            if (file_exists($file)) {
+        if (strpos($class, __NAMESPACE__) === 0) { // Verif si la class appartient au namespace App
+            $class = str_replace(__NAMESPACE__ . '\\', '', $class); // Supp "App\" du nom complet de la class
+            $class = str_replace('\\', '/', $class); // Remplace \ du namespace en / pour former le chemin du fichier (Controllers/Controller.php)
+            $file = __DIR__ . '/' . $class . '.php'; // Construit le chemin vers le bon fichier PHP a partir du nom de classe
+            if (file_exists($file)) { // SI fichier existe, on l'inclut SINON log utile pour debug
                 require_once $file;
             } else {
                 error_log("autoloader error $file");
--- Public/assets/js/modals.js
+++ Public/assets/js/modals.js
@@ -1,6 +1,5 @@
-// partie FUTTER mentions legales...
-
-function openModal(modalId) {
+// partie FUTTER mentions legales + rgpd
+function openModal(modalId) { // ouvrir une modale ou fermer avec close
   document.getElementById(modalId).style.display = "block";
 }
 
@@ -8,9 +7,9 @@ function closeModal(modalId) {
   document.getElementById(modalId).style.display = "none";
 }
 
-// Fermer la modale si l'utilisateur clique en dehors de celle-ci
+// Fermer la modale si l'utilisateur clique en dehors de la fenetre
 window.addEventListener("click", (event) => {
-  const modals = document.getElementsByClassName("modal");
+  const modals = document.getElementsByClassName("modal"); // List des modales parcourus ac boucle for sur tte les modales et se ferme au clic exterieur
   for (let i = 0; i < modals.length; i++) {
     if (event.target == modals[i]) {
       modals[i].style.display = "none";
@@ -19,27 +18,27 @@ window.addEventListener("click", (event) => {
 });
 
 // Partie Gestion des étoiles REVIEWS
-document.querySelectorAll('.modal .rating input[type="radio"]').forEach((input) => {
+document.querySelectorAll('.modal .rating input[type="radio"]').forEach((input) => { // Sélect touts les buttons radio
   input.addEventListener("change", () => {
     const value = parseInt(input.value); // Récupère la valeur de l'étoile sélectionnée
     const labels = Array.from(document.querySelectorAll(".modal .rating label")); // Sélectionne les labels
 
-    // Réinitialise toutes les étoiles
+    // MAJ affichage avec étoiles pleines ou vides
     labels.forEach((label, index) => {
       if (index + 1 <= value) {
-        label.classList.add("star-active"); // Ajoute la classe active
-        label.classList.remove("star-inactive"); // Supprime la classe inactive
+        label.classList.add("star-active");
+        label.classList.remove("star-inactive"); 
       } else {
-        label.classList.add("star-inactive"); // Ajoute la classe inactive
-        label.classList.remove("star-active"); // Supprime la classe active
+        label.classList.add("star-inactive"); 
+        label.classList.remove("star-active"); 
       }
     });
   });
 });
 
-// Enregistre la note sélectionnée ds l'input caché
+// Enregistre la note sélectionnée ds l'input caché pour la recup au submit
 document.querySelectorAll(".modal .rating input").forEach((input) => {
   input.addEventListener("change", function () {
-    document.querySelector("#note").value = this.value; // MAJ la valeur cachée
+    document.querySelector("#note").value = this.value;
   });
 });
